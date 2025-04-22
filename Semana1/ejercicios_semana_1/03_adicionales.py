9695f5493a5b25bd0d8afad2356f2c146ce4bbf7
miguel_990624 <miguelangelmolinagutierrez@gmail.com>
2025-04-22 13:12:45 -0500

    ejercicio 03 adicionales completado

--- Semana1/ejercicios_semana_1/03_adicionales.py
+++ Semana1/ejercicios_semana_1/03_adicionales.py
@@ -40,3 +40,64 @@ elif(genero == "M"):
     jubilar(edad,60)
 else:
     print("genero no valido")
+
+
+"""
+3. Calculadora de salario neto
+"""
+salario=int(input("ingresa tu salario: "))
+descuento= int(input("ingresa tu descuento mensual: "))
+print(f"tu salario tras reducciones es de {salario -(salario*descuento)/100}")
+
+"""
+4. ¿Compatibles?
+"""
+nombres = []
+edades = []
+print("veamos que tan compatibles son")
+for x in range(2):
+    nombre = input(f"ingresa el nombre {x+1}: ")
+    edad = int(input(f"ahora, {nombre} ingresa tu edad: "))
+    nombres.append(nombre)
+    edades.append(edad)
+
+if(nombres[0] != nombres[1]):
+    if(edades[0]>=18 and edades[1]>= 18):
+        if(10 > edades[0] - edades[1] > -10):
+            print("son compatibles")
+        else:
+            print("no son compatibles")
+    else:
+        print("no son compatibles")
+else:
+    print("no son compatibles")
+
+"""
+5. Calculadora de múltiplos
+"""
+num1 = int(input("ingrese un numero que quiera conocer si tiene multiplo: "))
+num2 = int(input("ingrese el numero que cree que es multiplo de su anterio opcion: "))
+if(num1 % num2 == 0):
+    print(f"{num1} es multiplo de {num2}")
+else:
+    print("no son multiplos")
+
+"""
+6. Número mágico
+"""
+num1 = int(input("ingrese un numero: "))
+if(num1 % 3 == 0 and num2 % 5 == 0):
+    print("FizzBuzz")
+elif(num1 % 3 == 0):
+    print("Fizz")
+elif(num1 % 3 == 0):
+    print("Buzz")
+else:
+    print("Numero magico")
+
+
+"""
+7. Conversor de unidades avanzado
+"""
+num1=float(input("ingrese un numero de kilometros"))
+print(f"{num1}km = {num1*1000}metros, {num1*100000}cm, {num1*1000000}mm")
