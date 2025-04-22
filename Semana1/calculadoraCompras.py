1ba9b5354423c7af5f06503ae863f80e72ff1fc6
miguel_990624 <miguelangelmolinagutierrez@gmail.com>
2025-04-22 10:39:48 -0500

    organice la documentacion en el entregable de semana 1 y termine 01_operadores

--- Semana1/calculadoraCompras.py
+++ Semana1/calculadoraCompras.py
@@ -1,12 +1,12 @@
 """
 El ejercicio esta en mi repositorio: https://github.com/miguel-990624/Modulo1
 """
-
 print("Calculadora de compra")
 nombre = input("ingrese el nombre del producto deseado: ")
 unitario = input("ingrese el valor unitario del producto deseado: ")
 cantidad = input("ingrese la cantidad de productos deseados, en numeros enteros: ")
 descuento = input("ingrese el valor de su descuento entre el 0 y 100, si aplica: ")
+
 """
 Cree una funcion que me calcula el valor de un descuento para llamarla dentro del codigo
 si el cliente ingresa bien los datos
@@ -15,13 +15,12 @@ def calcular () :
     total = unitario * cantidad
     totalDescontado = ((100 - descuento)/100) * total
     print(f"Para el producto {nombre}, el precio total es de: ${total:.2f}, con el valor del descuento ${totalDescontado:.2f}")
+    
 """
 En stack overflow encontre que lo mas normal es EAFP, que seria usar el metodo try, except, pero tambien
 ponian que existe el metodo replace, que me va a buscar el punto decimal, lo va a quitar, y solo lo va a hacer 1 vez
 de modo que cuando el cliente ingrese un numero asi tenga un decimal, lo va a reconocer como numero
 por que tambien estoy usando el metodo isdigit para verificar que sea un valor numerico.
-https://stackoverflow.com/questions/4138202/using-isdigit-for-floats
-https://www.w3schools.com/python/ref_string_replace.asp
 """
 if(unitario.replace(".", "", 1).isdigit()):
     """
@@ -57,3 +56,9 @@ if(unitario.replace(".", "", 1).isdigit()):
         print("Cantidad de productos no valida, no es un numero")
 else:
     print("Valor de precio unitario no valido, no es un numero")
+"""
+Documentacion:
+https://stackoverflow.com/questions/4138202/using-isdigit-for-floats
+https://www.w3schools.com/python/ref_string_replace.asp
+https://www.freecodecamp.org/news/python-convert-string-to-int-how-to-cast-a-string-in-python/
+"""
--- Semana1/ejercicios_semana_1/01_operadores_aritmeticos.py
+++ Semana1/ejercicios_semana_1/01_operadores_aritmeticos.py
@@ -62,3 +62,99 @@ print(f"El valor total de tu division es: {num1//num2}, con un residuo de {num1%
 num1=int(input("ingresa el valor de la primera cifra para realizar todos los procesos matematicos con ella, este sera el numerador: "))
 num2=int(input("ingresa el valor de la segunda cifra para realizar todos los procesos matematicos con ella, este sera el denominador: "))
 print(f"El valor de la suma es de {num1+num2}, el de la resta es de {num1-num2}, multiplicacion es {num1*num2}, division es {num1/num2}")
+
+"""
+10. Potencias con f-strings
+"""
+num1=int(input("Escribe el numero del que quieres conocer la potencia: "))
+print(f"la potencia 2 de {num1} es: {num1**2}, y su potencia 3 es: {num1**3}")
+
+"""
+11. Parte entera de un decimal
+"""
+num1=float(input("ingresa un numero decimal: "))
+print(f"el numero sin decimales es {num1:.0f}")
+
+"""
+12. Mayor de edad (sin condicional)
+"""
+num1=int(input("ingresa tu edad: "))
+print("Eres mayor de edad?")
+print(num1 >= 18)
+
+"""
+13. ¿Son iguales?
+"""
+num1=int(input("ingresa un numero: "))
+num2=int(input("ingresa un segundo numero: "))
+if(num1 == num2):
+    print("son iguales")
+else:
+    print("no son iguales")
+
+"""
+14. ¿Mayor que?
+"""
+num1 = int(input("ingresa un numero: "))
+num2 = int(input("ingresa un segudno numero: "))
+if (num1 > num2):
+    print(f"{num1} es mayor que {num2}")
+else:
+    print(f"{num1} no es mayor que {num2}")
+
+"""
+15. ¿Menor o igual?
+"""
+num1 = int(input("ingresa un numero: "))
+num2 = int(input("ingresa un segudno numero: "))
+if (num1 <= num2):
+    print(f"{num1} es menor o igual que {num2}")
+else:
+    print(f"{num1} no es menor o igual que {num2}")
+
+"""
+16. Ambos mayores que 10
+"""
+num1 = int(input("ingresa un numero: "))
+num2 = int(input("ingresa un segudno numero: "))
+if (num1 < 10 and num2 < 10):
+    print("son mayores de 10")
+else:
+    print("no son mayores de 10")
+
+"""
+17. Al menos uno mayor que 10
+"""
+num1 = int(input("ingresa un numero: "))
+num2 = int(input("ingresa un segudno numero: "))
+if (num1 < 10 or num2 < 10):
+    print("al menos 1 es mayor de 10")
+else:
+    print("no son mayores de 10")
+
+"""
+18. No son iguales
+"""
+num1 = int(input("ingresa un numero: "))
+num2 = int(input("ingresa un segudno numero: "))
+if (not num1 == num2):
+    print(f"{num1} no es igual que {num2}")
+else:
+    print(f"{num1} es igual que {num2}")
+
+"""
+19. Promedio
+"""
+num1 = int(input("ingresa un numero: "))
+num2 = int(input("ingresa un segudno numero: "))
+num3 = int(input("ingresa un tercer numero: "))
+print(f"el promedio de tus numeros es {(num1 +num2 + num3)/3}")
+
+"""
+20. Divisible por 5
+"""
+num1 = int(input("ingresa un numero: "))
+if(num1%5 == 0):
+    print("es divisible por 5")
+else:
+    print("no es divisible por 5")
